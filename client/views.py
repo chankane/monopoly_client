76bc23cd746d299aec7575a7da17c6823a9a3cf3
fj181435 <kanegawa.masaya@jp.fujitsu.com>
2019-01-08 14:52:30 +0900

    Hello, World from HTML

--- client/views.py
+++ client/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home(request):
-    return HttpResponse("Hello, World")
+def home(req):
+    # return HttpResponse("Hello, World")
+    return render(req, 'index.html')
